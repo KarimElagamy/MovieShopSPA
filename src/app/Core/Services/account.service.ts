e0ba93153afff6101cd6bc7f7380054fac57476b
KarimElagamy <102981217+KarimElagamy@users.noreply.github.com>
2022-07-07 21:16:32 -0400

    Today's code push

--- src/app/Core/Services/account.service.ts
+++ src/app/Core/Services/account.service.ts
@@ -1,17 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Login } from 'src/app/Shared/Models/login';
+import { Register } from 'src/app/Shared/Models/register';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  Login(){
-
+  Login(login:Login):Observable<boolean>{
+    return this.http.post('https://localhost:7079/api/Account/login', login).pipe(map((response:any) => {
+      if (response){
+        localStorage.setItem('token', response.token);
+        return true;
+      }
+      return false;
+    }))
   }
 
   Logout(){
-    
+    localStorage.removeItem('token');
   }
+
+  Register(registration:Register):Observable<boolean>{
+    return this.http.post<boolean>('https://localhost:7079/api/Account/register', registration);
+  }
+
 }
+
