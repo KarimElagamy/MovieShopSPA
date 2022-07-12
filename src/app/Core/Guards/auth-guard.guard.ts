de3203fe2d12e751c8aaf39f7843297eae2bbc91
KarimElagamy <102981217+KarimElagamy@users.noreply.github.com>
2022-07-12 14:11:44 -0400

    update

--- src/app/Core/Guards/auth-guard.guard.ts
+++ src/app/Core/Guards/auth-guard.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AccountService } from '../Services/account.service';
 
 @Injectable({
@@ -11,17 +11,15 @@ export class AuthGuardGuard implements CanActivate {
   isLoggedIn:boolean = false;
   constructor(private accountService:AccountService){ }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
       this.accountService.isLoggedIn.subscribe(data => {
         this.isLoggedIn = data;
       });
     if (localStorage.getItem('token') != null && this.isLoggedIn){
-      return true;
+      return of(true);
     }
     else {
-      return false;
+      return of(true);
     }
   };
   
